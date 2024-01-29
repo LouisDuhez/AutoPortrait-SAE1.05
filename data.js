d642734d5f539d1f0414bc052520dcba284ebd6f
LouisDuhez <158026663+LouisDuhez@users.noreply.github.com>
2024-01-29 08:58:42 +0100

    SAE1.05
    Voici le projet du Portrait chinois

---
+++ data.js
@@ -0,0 +1,58 @@
+
+let data
+data = [
+    {
+    Question:"une Couleur",
+    Reponse:"la couleur <span class='rouge'>Rouge</span>",
+    explication:"Pour moi, la couleur rouge symbolise quelque chose de très conflictuelle. En effet, elle symbolise à la fois le danger et l'amour, car on l'assimile à la fois au sang, mais aussi au cœur. C'est pour cela que je trouve cette couleur magnifique, car elle arrive à exprimer deux sentiments totalement opposés.",
+    image : 'image/Earth.png alt="Image de la terre"',
+}
+,
+{
+    Question:"Un organe",
+    Reponse:"<span class='rouge'>un œil</span>",
+    explication :" Pour moi les yeux montre tellement d'une personne, que ce soit sa personnalité, ses émotions. Je trouve ça aussi tellement beau que j'adore les dessiner, même si je n'arrive pas à reproduire correctement ce que un œil dégage, j'essaie de faire de mon mieux et un jour j'y arriverai j'en suis sûr !",
+    image:'image/Eyes.png alt="illustration de visage"',
+
+}
+,
+{
+    Question:"un animal",
+    Reponse:"<span class='rouge'>un poisson</span>",
+    explication :"J'ai toujours aimé les poissons, je trouve ça très mignon et très majestueux. De plus j'ai toujours aimé les fonds marins je trouve qu'il y a tellement de choses magnifiques que l'on a sûrement jamais découvertes. Mais malheureusement, nous sommes en train de détruire tout ça, alors j’espère que nous arriverons à les sauver ",
+    image :'image/Fish.png alt="Un poisson rouge"',
+}
+,
+{
+    Question:"un film",
+    Reponse:"<span class='rouge'>Star Wars III</span>",
+    explication :"Stars wars, pour moi et pour beaucoup est une saga qui a bercé mon enfance, je me souviens la première fois que j'ai commencé le premier film et j'ai tout de suite adoré. Alors comment parler de cette magnifique saga sans parler du meilleur épisode, la revanche des Sith. Ce film j'ai dû le voir au moins une vingtaine de fois plus petit et je ne m'en lasse jamais les musiques, les combats tout est parfait pour moi dans ce film.",
+    image: 'image/Movie.png alt="illustration repésentant le cinéma"',
+}
+,
+{
+
+    Question : "un jeu vidéo",
+    Reponse : "<span class='rouge'>Hollow Knight</span>",
+    explication : "Pour moi Hollow knight symbolise ce que l'industrie du jeu vidéo a perdus. Un jeu vidéo doit raconter une histoire sans la raconter. Oui, je ne dis pas que le lore doit tout le temps être crypté loin de là, mais il est important de découvrir pour nous même l'histoire d'un jeu, ses spécificités. Car c'est comme ça qu'un jeu nous fait vibrer et qu'on va l'aimer.",
+    image :"image/hollowknight.jpg alt=' Image de Hollow knight'",
+}
+,
+{
+
+    Question : "de la nourriture",
+    Reponse : "de la <span class='rouge'>Pizza</span>",
+    explication : "J'aime beaucoup la pizza, déjà le fait qu'il y en ait de toute sorte, que l'on peut en manger partout. Pour moi, c'est devenu une valeur sûre. De plus j'ai l'habitude d'en faire avec mon père, ce qui m'a fait encore plus l'aimer, car elles sont franchement super bonnes !",
+    image :"image/Pizza.png alt='Un pizzaiolo'",
+}
+,
+{
+
+    Question : "moi",
+    Reponse : "<span class='rouge'>Qui</span> plus tard ?",
+    explication : "C'est vrai, durant cette exercice, nous nous demandons qui nous serons si nous étions ça. Mais si j'étais moi qui serais-je dans 5-10 ans. et bien je vais essayer d'y répondre, je pense être soit un développé aguerri qui arrive à réaliser des beaux projets que ce soit en HTML/CSS JavaScript. Ou peut être un créatif, je pourrais être un motion designer. Qui sait ?, je pourrais être beaucoup de chose.<br><br> Seul l'avenir nous le dira",
+    image :"image/Problem.png alt=' Une personne qui réfléchi pour résoudre un problème'",
+}
+]
+
+
---
+++ script.js
@@ -0,0 +1,135 @@
+
+var numCase = 0 
+    // instructions Javascript exécutées après chargement du DOM
+var parent=document.querySelector(".listAnalogies")
+console.log(parent)
+
+
+// création d'un curseur personalisé : 
+// on prend l'événement de X et Y pour changer leurs valeurs tout au long du déplacement de la souris
+let cursor = document.querySelector('.cursor')
+document.addEventListener('mousemove', e=> {
+    cursor.setAttribute('style', 'top:'+(e.pageY -20)+"px; left:"+ (e.pageX -20)+"px;")
+})
+document.addEventListener('click', ()=>{
+    cursor.classList.add('expand');
+
+    setTimeout(()=>{
+        cursor.classList.remove('expand');
+    },500);
+})
+
+// code pour créer les analogies avec deux classes ( analogie et analogie + numéro de l'analogies pour pouvoir les personalisers si besoin)
+data.forEach(function(element) {
+    console.log(numCase)
+    console.log("Si j'étais " + element + " je serais")
+    parent.innerHTML = parent.innerHTML + "<section id='analogie"+numCase+"'class ='analogie analogie"+ numCase +"'><h2>Si j'étais " + data[numCase].Question + ",<br> je serais " +data[numCase].Reponse +"</h2> <img src="+ data[numCase].image+"><p>"+ data[numCase].explication + "</p></section>" 
+    numCase = numCase + 1 
+    
+    
+    }
+)
+;
+
+// Tentative d'animation pour scroll de section en section.
+
+/*var topProfil
+var profil = document.querySelector(".moi")
+profil.addEventListener("click", function(element){
+    topProfil=document.querySelector(".profil").offsetTop
+    window.scrollTo({
+        top:topProfil,
+        behavior:"smooth",
+    })
+})
+var topWindow
+var flecheHaut= document.querySelector(".toTop")
+flecheHaut.addEventListener("click",function(event){
+    topWindow = window.offsetTop
+    if (topWindow ==1031){
+        window.scrollTo({
+            top:0,
+            behavior:"smooth",
+        })
+    }
+})
+*/
+
+
+// code pour le pop up : 
+// ici on affiche le popup après le click sur le "Mentions légales"
+var afficheimage = document.querySelector(".Mentions")
+afficheimage.addEventListener("click",function(event){
+    document.querySelector(".popup-invisible").classList.add('popup-visible')
+    document.querySelector(".popup-invisible").classList.remove('popup-invisible')
+
+
+})
+// ici on cache le popup après le click sur "fermer"
+var cachefenetre = document.querySelector(".cache-fenetre")
+.addEventListener("click",function(event){
+    
+    document.querySelector(".popup").classList.add('popup-invisible')
+    document.querySelector("popup").classList.remove('popup-visible')
+
+})
+// ici le code pour récupérer les information du formulaire.
+// ici on récupère le contenue pour l'analogie
+let analogies = document.querySelector("#analogies")
+let analogiesKey
+analogies.addEventListener("keyup", function(event){
+    analogiesKey = analogies.value
+    console.log(analogiesKey)
+    return analogiesKey
+})
+// ici on récupère la réponse pour l'analogie
+let valeurAnalogies = document.querySelector("#valeurAnalogies")
+let valeurKey
+valeurAnalogies.addEventListener("keyup",function(event){
+   
+    valeurKey = valeurAnalogies.value
+    console.log(valeurKey)
+    return valeurKey
+})
+// ici on récupère l'explication de l'analogie
+let explicationAnalogie = document.querySelector("#explicationAnalogies")
+let explicationKey
+explicationAnalogie.addEventListener("keyup",function(event){
+   
+    explicationKey = explicationAnalogie.value
+    console.log(explicationKey)
+    return explicationKey
+})
+// ici on récupère l'image pour l'analogie
+let imageAnalogie = document.querySelector("#imageAnalogies")
+let imageKey
+imageAnalogie.addEventListener("keyup",function(event){
+   
+    imageKey = imageAnalogie.value
+    console.log(imageKey)
+    return imageKey
+})
+// ici on crée la nouvelle analogie à partir des élément collectés
+let formulaireAnalogie = document.querySelector(".analogiesFormulaire")
+envoyerForm = document.querySelector("#submit")
+envoyerForm.addEventListener("click", function(event){
+    event.preventDefault()
+    formulaireAnalogie.innerHTML = formulaireAnalogie.innerHTML + "<section class ='analogie analogie"+numCase+"'><h2>Si j'étais " + analogiesKey + ", je serais <span class='rouge'>"+valeurKey+"</span></h2><img src=" + imageKey + "> <p>" + explicationKey + "</p></section>" 
+
+})
+// ici on envoie les informations collectés à l'API pour ensuite les stocker.
+let urlVisitee = "https://perso-etudiant.u-pem.fr/~gambette/portrait/api.php?format=json&login=louis.duhez&courriel=philippe.gambette@u-pem.fr&message=Si j'étais "+analogiesKey+ "Je serais" +valeurKey+"explication:"+explicationKey+"lien image :"+imageKey 
+let submit=document.querySelector("#submit")
+submit.addEventListener("click", function(event){
+    fetch(urlVisitee).then(function(response){
+        response.json().then(function(data){
+            console.log("Réponse reçue : ")
+            console.log(data);
+        })  
+    }) 
+})
+
+
+
+
+
