04fbd1437e9e7f5736c3fb357116d53cafab02c8
LouisDuhez <158026663+LouisDuhez@users.noreply.github.com>
2024-02-28 00:22:03 +0100

    V2

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
-
-var numCase = 0 
-    // instructions Javascript exécutées après chargement du DOM
+//Sélection de la section "listAnalogies" pour pouvoir y incorporer les Analogies
 var parent=document.querySelector(".listAnalogies")
 console.log(parent)
-
 
 // création d'un curseur personalisé : 
 // on prend l'événement de X et Y pour changer leurs valeurs tout au long du déplacement de la souris
@@ -18,12 +15,15 @@ document.addEventListener('click', ()=>{
         cursor.classList.remove('expand');
     },500);
 })
-
 // code pour créer les analogies avec deux classes ( analogie et analogie + numéro de l'analogies pour pouvoir les personalisers si besoin)
+
+//Création d'une variable pour pouvoir stocker le nombre de l'analogies après chaque tour de la fonction
+var numCase = 0 
 data.forEach(function(element) {
-    console.log(numCase)
-    console.log("Si j'étais " + element + " je serais")
-    parent.innerHTML = parent.innerHTML + "<section id='analogie"+numCase+"'class ='analogie analogie"+ numCase +"'><h2>Si j'étais " + data[numCase].Question + ",<br> je serais " +data[numCase].Reponse +"</h2> <img src="+ data[numCase].image+"><p>"+ data[numCase].explication + "</p></section>" 
+    //console.log(numCase)
+    //console.log("Si j'étais " + element + " je serais")
+    parent.innerHTML = parent.innerHTML + 
+    "<section id='analogies" + numCase + "'class ='analogies_bloc'><div class='analogies_explication'><h2>Si j'étais " + data[numCase].Question +"<br>Je serai " + data[numCase].Reponse + " </h2> <br> <p>"+data[numCase].explication+"</p></div><div class='analogies_images'><img src=" + data[numCase].image + "></div></section>"
     numCase = numCase + 1 
     
     
@@ -31,50 +31,6 @@ data.forEach(function(element) {
 )
 ;
 
-// Tentative d'animation pour scroll de section en section.
-
-/*var topProfil
-var profil = document.querySelector(".moi")
-profil.addEventListener("click", function(element){
-    topProfil=document.querySelector(".profil").offsetTop
-    window.scrollTo({
-        top:topProfil,
-        behavior:"smooth",
-    })
-})
-var topWindow
-var flecheHaut= document.querySelector(".toTop")
-flecheHaut.addEventListener("click",function(event){
-    topWindow = window.offsetTop
-    if (topWindow ==1031){
-        window.scrollTo({
-            top:0,
-            behavior:"smooth",
-        })
-    }
-})
-*/
-
-
-// code pour le pop up : 
-// ici on affiche le popup après le click sur le "Mentions légales"
-var afficheimage = document.querySelector(".Mentions")
-afficheimage.addEventListener("click",function(event){
-    document.querySelector(".popup-invisible").classList.add('popup-visible')
-    document.querySelector(".popup-invisible").classList.remove('popup-invisible')
-
-
-})
-// ici on cache le popup après le click sur "fermer"
-var cachefenetre = document.querySelector(".cache-fenetre")
-.addEventListener("click",function(event){
-    
-    document.querySelector(".popup").classList.add('popup-invisible')
-    document.querySelector("popup").classList.remove('popup-visible')
-
-})
-// ici le code pour récupérer les information du formulaire.
-// ici on récupère le contenue pour l'analogie
 let analogies = document.querySelector("#analogies")
 let analogiesKey
 analogies.addEventListener("keyup", function(event){
@@ -114,7 +70,7 @@ let formulaireAnalogie = document.querySelector(".analogiesFormulaire")
 envoyerForm = document.querySelector("#submit")
 envoyerForm.addEventListener("click", function(event){
     event.preventDefault()
-    formulaireAnalogie.innerHTML = formulaireAnalogie.innerHTML + "<section class ='analogie analogie"+numCase+"'><h2>Si j'étais " + analogiesKey + ", je serais <span class='rouge'>"+valeurKey+"</span></h2><img src=" + imageKey + "> <p>" + explicationKey + "</p></section>" 
+    formulaireAnalogie.innerHTML = formulaireAnalogie.innerHTML + "<section class ='analogies_bloc'><div class='analogies_explication'<h2>Si j'étais " + analogiesKey + ", je serais <span class='rouge'>"+valeurKey+"</span> <p>" + explicationKey + "</p></h2></div><div class='analogies_images'><img src=" + imageKey + "></section>" 
 
 })
 // ici on envoie les informations collectés à l'API pour ensuite les stocker.
@@ -128,8 +84,3 @@ submit.addEventListener("click", function(event){
         })  
     }) 
 })
-
-
-
-
-
